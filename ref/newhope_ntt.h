3231ce5583fc669f7571caaa2973f95da4d8da1e
cothan <cothannguyen@gmail.com>
2020-07-08 21:35:19 -0400

    support constants

--- ref/newhope_ntt.h
+++ ref/newhope_ntt.h
@@ -7,6 +7,14 @@ extern uint16_t omegas_inv_bitrev_montgomery[];
 extern uint16_t gammas_bitrev_montgomery[];
 extern uint16_t gammas_inv_montgomery[];
 
+extern uint16_t my_gammas_bitrev[];
+extern uint16_t my_gammas_inv[];
+
+extern uint16_t my_omegas_inv[];
+extern uint16_t my_omegas[];
+
+extern uint16_t my_omegas_inv_bitrev[];
+
 void bitrev_vector(uint16_t* poly);
 void mul_coefficients(uint16_t* poly, const uint16_t* factors);
 void ntt(uint16_t* poly, const uint16_t* omegas);
