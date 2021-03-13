fefec2fd1c915f0a0a25881dc8a1b8a777204716
cothan <cothannguyen@gmail.com>
2021-03-13 08:43:24 -0500

    clean up

--- dilithium/util.c
+++ dilithium/util.c
@@ -11,3 +11,27 @@ void print_array(int32_t *a, int bound, const char *string)
     printf("\n");
 }
 
+void print_reshaped_array(bram *ram, int bound, const char *string)
+{
+    printf("%s :\n", string);
+    for (int i = 0; i < bound; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%d, ", ram->vec[i].coeffs[j]);
+        }
+    }
+    printf("\n");
+}
+
+// Store 4 coefficients per line
+void reshape(bram *ram, int32_t in[N])
+{
+    for (int i = 0; i < N / 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            ram->vec[i].coeffs[j] = in[4 * i + j];
+        }
+    }
+}
