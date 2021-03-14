c415a3bc76fb6f08f10b587e4c62dc16a5184b48
cothan <cothannguyen@gmail.com>
2021-03-14 08:37:46 -0400

    update, todo: forward ntt

--- dilithium/ntt2x2.c
+++ dilithium/ntt2x2.c
@@ -17,9 +17,9 @@
 
 #define LOG_N  8 // N= 256, => log(N) = 8
 
-enum OP {NTT_MODE, MUL_MODE, DECODE_TRUE, DECODE_FALSE};
+enum OP {NTT_MODE, INVNTT_MODE, MUL_MODE, DECODE_TRUE, DECODE_FALSE};
 
-static const int32_t zetas[N] = {
+/* static const int32_t zetas[N] = {
          0,    25847, -2608894,  -518909,   237124,  -777960,  -876248,   466468,
    1826347,  2353451,  -359251, -2091905,  3119733, -2884855,  3111497,  2680103,
    2725464,  1024112, -1079900,  3585928,  -549488, -1119584,  2619752, -2108549,
@@ -52,6 +52,41 @@ static const int32_t zetas[N] = {
    -426683,  1723600, -1803090,  1910376, -1667432, -1104333,  -260646, -3833893,
   -2939036, -2235985,  -420899, -2286327,   183443,  -976891,  1612842, -3545687,
    -554416,  3919660,   -48306, -1362209,  3937738,  1400424,  -846154,  1976782
+};
+ */
+static const int32_t zetas_barret[N] = {
+        0, -3572223,  3765607,  3761513, -3201494, -2883726, -3145678, -3201430, 
+  -601683,  3542485,  2682288,  2129892,  3764867, -1005239,   557458, -1221177, 
+  -3370349, -4063053,  2663378, -1674615, -3524442,  -434125,   676590, -1335936, 
+  -3227876,  1714295,  2453983,  1460718,  -642628, -3585098,  2815639,  2283733, 
+  3602218,  3182878,  2740543, -3586446, -3110818,  2101410,  3704823,  1159875, 
+    394148,   928749,  1095468, -3506380,  2071829, -4018989,  3241972,  2156050, 
+  3415069,  1759347,  -817536, -3574466,  3756790, -1935799, -1716988, -3950053, 
+  -2897314,  3192354,   556856,  3870317,  2917338,  1853806,  3345963,  1858416, 
+  3073009,  1277625, -2635473,  3852015,  4183372, -3222807, -3121440,  -274060, 
+  2508980,  2028118,  1937570, -3815725,  2811291, -2983781, -1109516,  4158088, 
+  1528066,   482649,  1148858, -2962264,  -565603,   169688,  2462444, -3334383, 
+  -4166425, -3488383,  1987814, -3197248,  1736313,   235407, -3250154,  3258457, 
+  -2579253,  1787943, -2391089, -2254727,  3482206, -4182915, -1300016, -2362063, 
+  -1317678,  2461387,  3035980,   621164,  3901472, -1226661,  2925816,  3374250, 
+  1356448, -2775755,  2683270, -2778788, -3467665,  2312838,  -653275,  -459163, 
+    348812,  -327848,  1011223, -2354215, -3818627, -1922253, -2236726,  1744507, 
+      1753, -1935420, -2659525, -1455890,  2660408, -1780227,   -59148,  2772600, 
+  1182243,    87208,   636927, -3965306, -3956745, -2296397, -3284915, -3716946, 
+    -27812,   822541,  1009365, -2454145, -1979497,  1596822, -3956944, -3759465, 
+  -1685153, -3410568,  2678278, -3768948, -3551006,   635956,  -250446, -2455377, 
+  -4146264, -1772588,  2192938, -1727088,  2387513, -3611750,  -268456, -3180456, 
+  3747250,  2296099,  1239911, -3838479,  3195676,  2642980,  1254190,   -12417, 
+  2998219,   141835,   -89301,  2513018, -1354892,   613238, -1310261, -2218467, 
+  -458740, -1921994,  4040196, -3472069,  2039144, -1879878,  -818761, -2178965, 
+  -1623354,  2105286, -2374402, -2033807,   586241, -1179613,   527981, -2743411, 
+  -1476985,  1994046,  2491325, -1393159,   507927, -1187885,  -724804, -1834526, 
+  -3033742,  -338420,  2647994,  3009748, -2612853,  4148469,   749577, -4022750, 
+  3980599,  2569011, -1615530,  1723229,  1665318,  2028038,  1163598, -3369273, 
+  3994671,   -11879, -1370517,  3020393,  3363542,   214880,   545376,  -770441, 
+  3105558, -1103344,   508145,  -553718,   860144,  3430436,   140244, -1514152, 
+  -2185084,  3123762,  2358373, -2193087, -3014420, -1716814,  2926054,  -392707, 
+  -303005,  3531229, -3974485, -3773731,  1900052,  -781875,  1054478,  -731434, 
 };
 
 int32_t MUL_RAM[N] = {
@@ -89,6 +124,41 @@ int32_t MUL_RAM[N] = {
     41978, 41978, 41978, 41978, 41978, 41978, 41978, 41978, 
 };
 
+int32_t MUL_RAM_barret[N] = {
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+    16382, 16382, 16382, 16382, 16382, 16382, 16382, 16382, 
+};
+
 // This function will output an element when insert 1 element
 int32_t FIFO(const int dept, int32_t *fifo, const int32_t new)
 {
@@ -112,44 +182,61 @@ void swap(int32_t *a, int32_t *b)
 }
 
 
-/* This code copied from butterfly unit in Dilithium Forward NTT
-t = montgomery_reduce((int64_t)zeta * a[j + len]);
-a[j + len] = a[j] - t;
-a[j] = a[j] + t; */
-void forward_butterfly(int32_t *bj, int32_t *bjlen, const int32_t zeta, int32_t aj, int32_t ajlen)
+void butterfly(int mode, int32_t *bj, int32_t *bjlen, const int32_t zeta, int32_t aj, int32_t ajlen)
 {
     int32_t t;
 
-    t = montgomery_reduce((int64_t)zeta * ajlen);
-    ajlen = aj - t;
-    aj = aj + t;
-    *bj = aj;
-    *bjlen = ajlen;
-}
+    int32_t aj_copy, ajlen_copy;
+    aj_copy = aj;
+    ajlen_copy = ajlen;
 
-
-/* This code copied from butterfly unit in Dilithium Inverse NTT
-t = a[j];
-a[j] = t + a[j + len];
-a[j + len] = t - a[j + len];
-a[j + len] = montgomery_reduce((int64_t)zeta * a[j + len]); */
-void inverse_butterfly(int mode, int32_t *bj, int32_t *bjlen, const int32_t zeta, int32_t aj, int32_t ajlen)
-{
-    int32_t t;
-
+    if (mode == INVNTT_MODE)
+    {
+        /* This code copied from butterfly unit in Dilithium Inverse NTT
+        t = a[j];
+        a[j] = t + a[j + len];
+        a[j + len] = t - a[j + len];
+        a[j + len] = montgomery_reduce((int64_t)zeta * a[j + len]); */
+        // INV_NTT
+        t = aj;
+        aj = t + ajlen;
+        ajlen = t - ajlen;
+    }
+    // MUL
+    // t = ajlen = montgomery_reduce((int64_t)zeta * ajlen);
+    t = ajlen = ((int64_t)zeta * ajlen) % Q;
+    
     if (mode == NTT_MODE)
     {
-        // NTT 
-        t = aj; 
-    }
-    else{
-        // MUL
-        t = ajlen << 1;
+        /* This code copied from butterfly unit in Dilithium Forward NTT
+        t = montgomery_reduce((int64_t)zeta * a[j + len]);
+        a[j + len] = a[j] - t;
+        a[j] = a[j] + t; */
+        // NTT
+        ajlen = aj_copy - t;
+        aj = aj_copy + t;
     }
 
-    aj = t + ajlen;
-    ajlen = t - ajlen;
-    ajlen = montgomery_reduce((int64_t) zeta * ajlen);
+    if (mode == INVNTT_MODE)
+    {
+      // if (aj & 1)
+      // {
+      //   aj = (aj >> 1) + (Q + 1) / 2;
+      // }
+      // else
+      // {
+      //   aj = (aj >> 1);
+      // }
+
+      // if (ajlen & 1)
+      // {
+      //   ajlen = (ajlen >> 1) + (Q + 1) / 2;
+      // }
+      // else
+      // {
+      //   ajlen = (ajlen >> 1);
+      // }
+    }
 
     *bj = aj;
     *bjlen = ajlen;
@@ -186,7 +273,7 @@ int addr_decoder(int addr_in)
     return my_map[addr_in];
 }
 
-void ntt2x2(bram *ram, int32_t *mul_ram, int mode, int decode)
+void ntt2x2(bram *ram, bram *mul_ram, int mode, int decode)
 {
     int32_t fifo_i[DEPT_I] = {0},
             fifo_a[DEPT_A] = {0},
@@ -210,7 +297,7 @@ void ntt2x2(bram *ram, int32_t *mul_ram, int mode, int decode)
     int32_t w1, w2, w3, w4;
     
 
-    if (mode == NTT_MODE)
+    if (mode == INVNTT_MODE)
     {
         bound = LOG_N;
     }
@@ -223,7 +310,7 @@ void ntt2x2(bram *ram, int32_t *mul_ram, int mode, int decode)
     {
         for (int j = 0; j < (1 << s); j++)
         {
-            if (mode == NTT_MODE)
+            if (mode == INVNTT_MODE)
             {
                 // NTT mode
                 // Layer s 
@@ -255,18 +342,23 @@ void ntt2x2(bram *ram, int32_t *mul_ram, int mode, int decode)
                     ram_i = addr;
                 }
 
-                if (mode == NTT_MODE)
+                if (mode == INVNTT_MODE)
                 {
-                    w1 = -zetas[i1];
-                    w2 = -zetas[i2];
-                    w3 = -zetas[i3];
-                    w4 = -zetas[i4];
+                    // w1 = -zetas[i1];
+                    // w2 = -zetas[i2];
+                    // w3 = -zetas[i3];
+                    // w4 = -zetas[i4];
+                    w1 = -zetas_barret[i1];
+                    w2 = -zetas_barret[i2];
+                    w3 = -zetas_barret[i3];
+                    w4 = -zetas_barret[i4];
                 }
                 else{
-                    w1 = mul_ram[i1];
-                    w2 = mul_ram[i2];
-                    w3 = mul_ram[i3];
-                    w4 = mul_ram[i4];
+                    // MUL
+                    w1 = mul_ram->vec[ram_i].coeffs[0];
+                    w2 = mul_ram->vec[ram_i].coeffs[1];
+                    w3 = mul_ram->vec[ram_i].coeffs[2];
+                    w4 = mul_ram->vec[ram_i].coeffs[3];
                 }
 
                 a = ram->vec[ram_i].coeffs[0];
@@ -287,8 +379,8 @@ void ntt2x2(bram *ram, int32_t *mul_ram, int mode, int decode)
                 save_a = a; 
                 save_c = c;
 
-                inverse_butterfly(mode, &a, &b, w1, a, b);
-                inverse_butterfly(mode, &c, &d, w2, c, d);
+                butterfly(mode, &a, &b, w1, a, b);
+                butterfly(mode, &c, &d, w2, c, d);
 
                 if (mode == MUL_MODE)
                 {
@@ -318,8 +410,8 @@ void ntt2x2(bram *ram, int32_t *mul_ram, int mode, int decode)
                     printf("==============================%d %d | %d %d\n", ram_i / 4, ram_i, j, k);
                 } */
 
-                inverse_butterfly(mode, &a, &b, w3, a, b);
-                inverse_butterfly(mode, &c, &d, w4, c, d);
+                butterfly(mode, &a, &b, w3, a, b);
+                butterfly(mode, &c, &d, w4, c, d);
 
                 if (mode == MUL_MODE)
                 {
@@ -349,7 +441,7 @@ void ntt2x2(bram *ram, int32_t *mul_ram, int mode, int decode)
 
                 if (write)
                 {
-                    if (mode == NTT_MODE)
+                    if (mode == INVNTT_MODE)
                     {
                         // Serial in Parallel out
                         switch (count & 0b11)
@@ -400,7 +492,7 @@ void ntt2x2(bram *ram, int32_t *mul_ram, int mode, int decode)
                     // writeback
                     // printf("[%d] <= (%d, %d, %d, %d)\n", fi, fa, fb, fc, fd);
                 }
-                if (mode == NTT_MODE)
+                if (mode == INVNTT_MODE)
                 {
                     // Only adjust omega in NTT mode
                     i1 -= w_m1;
@@ -423,7 +515,7 @@ void ntt2x2(bram *ram, int32_t *mul_ram, int mode, int decode)
         fd = FIFO(DEPT_D, fifo_d, 0);
         count++;
 
-        if (mode == NTT_MODE)
+        if (mode == INVNTT_MODE)
         {
             // Serial in Parallel out
             switch (count & 0b11)
@@ -486,14 +578,15 @@ void ntt2x2(bram *ram, int32_t *mul_ram, int mode, int decode)
 
 int ntt2x2_NTT(int32_t r_gold[N], int32_t r[N])
 {
-    bram ram;
+    bram ram, mul_ram;
     // Load data into BRAM, 4 coefficients per line
     reshape(&ram, r);
+    reshape(&mul_ram, MUL_RAM_barret);
     // Compute NTT
-    ntt2x2(&ram, MUL_RAM, NTT_MODE, DECODE_FALSE);
+    ntt2x2(&ram, &mul_ram, INVNTT_MODE, DECODE_FALSE);
     
     // Enable DECODE_TRUE only after NTT transform
-    ntt2x2(&ram, MUL_RAM, MUL_MODE, DECODE_TRUE);
+    ntt2x2(&ram, &mul_ram, MUL_MODE, DECODE_TRUE);
 
 
     // Run the reference code
@@ -539,21 +632,22 @@ int ntt2x2_NTT(int32_t r_gold[N], int32_t r[N])
             return 1;
         }
     }
-    // printf("==============NTT is Correct!\n\n");
+    printf("==============NTT is Correct!\n\n");
     return 0;
 }
 
 int ntt2x2_MUL(int32_t r_gold[N], int32_t r[N])
 {
-    bram ram;
+    bram ram, mul_ram;
+    
     // Load data into BRAM, 4 coefficients per line
     reshape(&ram, r);
-
+    reshape(&mul_ram, MUL_RAM);
 
     // MUL Operation using NTT
     // Enable DECODE_TRUE only after NTT transform
     // This example we only do pointwise multiplication
-    ntt2x2(&ram, MUL_RAM, MUL_MODE, DECODE_FALSE);
+    ntt2x2(&ram, &mul_ram, MUL_MODE, DECODE_FALSE);
     
     // Run the reference code
     pointwise_montgomery(r_gold, r_gold, MUL_RAM);
@@ -598,15 +692,16 @@ int ntt2x2_MUL(int32_t r_gold[N], int32_t r[N])
             return 1;
         }
     }
-    // printf("==============MUL is Correct!\n\n");
+    printf("==============MUL is Correct!\n\n");
     return 0;
 }
 
-#define TESTS 1000
+#define TESTS 1
 
 int main()
 {
-    srand(time(0));
+    // srand(time(0));
+    srand(0);
     int32_t r[N], r_gold[N], r_gold_copy[N], r_copy[N];
     int32_t t1, t2;
     int ret = 0;
@@ -634,11 +729,23 @@ int main()
         }
     }
 
+    ntt(r_gold);
+
     if (ret)
     {
         printf("ERROR\n");
     }
-    printf("OK\n");
+    else{
+      printf("OK\n");
+    }
 
     return ret;
 }
+
+/* 
+ * Combine GS and CT butterfly  unit: DONE 
+ * Replace Montgomery to Barrett reduction: DONE 
+ * Divide by 2 in inverse NTT: No yet 
+ * Find algorithm to compute fast bit-reverse permutation for Forward NTT: 
+ * What the point of merging forward and inverse butterfly unit?
+ */
