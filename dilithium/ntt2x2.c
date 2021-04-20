0ce49227ab27f9d0bfc65e4136f3b5393ff9852f
cothan <cothannguyen@gmail.com>
2021-04-20 18:15:15 -0400

    working forward NTT

--- dilithium/ntt2x2.c
+++ dilithium/ntt2x2.c
@@ -9,6 +9,8 @@
 #include "ram_util.h"
 #include "butterfly_unit.h"
 
+#define MAX(x, y) ((x < y) ? y : x);
+
 void ntt2x2(bram *ram, bram *mul_ram, enum OPERATION mode, enum MAPPING decode)
 {
     int32_t fifo_i[DEPT_I] = {0},
@@ -23,12 +25,12 @@ void ntt2x2(bram *ram, bram *mul_ram, enum OPERATION mode, enum MAPPING decode)
     int ram_i, bound, addr;
 
     int count = 0;
-    int write = 0;
+    int write_en = 0;
 
     const int w_m1 = 2;
     const int w_m2 = 1;
 
-    int32_t w1, w2, w3, w4;
+    int32_t w[4];
 
     switch (mode)
     {
@@ -71,6 +73,7 @@ void ntt2x2(bram *ram, bram *mul_ram, enum OPERATION mode, enum MAPPING decode)
                     }
                 }
 
+                // printf("%d - %d - %d - %d\n", i1, i2, i3, i4);
                 addr = k + j;
                 if (decode == DECODE_TRUE)
                 {
@@ -87,21 +90,21 @@ void ntt2x2(bram *ram, bram *mul_ram, enum OPERATION mode, enum MAPPING decode)
                     // w2 = -zetas[i2];
                     // w3 = -zetas[i3];
                     // w4 = -zetas[i4];
-                    w1 = -zetas_barret[i1];
-                    w2 = -zetas_barret[i2];
-                    w3 = -zetas_barret[i3];
-                    w4 = -zetas_barret[i4];
+                    w[0] = -zetas_barret[i1];
+                    w[1] = -zetas_barret[i2];
+                    w[2] = -zetas_barret[i3];
+                    w[3] = -zetas_barret[i4];
                 }
                 else
                 {
                     // MUL
-                    read_ram(&w1, &w2, &w3, &w4, mul_ram, ram_i);
+                    read_ram(&w[0], &w[1], &w[2], &w[3], mul_ram, ram_i);
                 }
 
                 read_ram(&a, &b, &c, &d, ram, ram_i);
 
                 // 2x2 circuit
-                buttefly_circuit(&a, &b, &c, &d, w1, w2, w3, w4, mode);
+                buttefly_circuit(&a, &b, &c, &d, w[0], w[1], w[2], w[3], mode);
 
                 // FIFO
                 FIFO(DEPT_A, fifo_a, a);
@@ -113,7 +116,7 @@ void ntt2x2(bram *ram, bram *mul_ram, enum OPERATION mode, enum MAPPING decode)
                 if (count == DEPT_I)
                 {
                     // FIFO_A is full
-                    write = 1;
+                    write_en = 1;
                     count = 0;
                 }
                 else
@@ -121,7 +124,7 @@ void ntt2x2(bram *ram, bram *mul_ram, enum OPERATION mode, enum MAPPING decode)
                     count++;
                 }
 
-                if (write)
+                if (write_en)
                 {
                     read_fifo(&fa, &fb, &fc, &fd, count, mode, fifo_a, fifo_b, fifo_c, fifo_d);
                     write_ram(ram, fi, fa, fb, fc, fd);
@@ -170,6 +173,12 @@ void ntt2x2(bram *ram, bram *mul_ram, enum OPERATION mode, enum MAPPING decode)
     */
 }
 
+int max(int a, int b)
+{
+    if (a < b)
+        return b;
+    return a;
+}
 
 #define DEPT_W 4
 
@@ -180,23 +189,24 @@ void forward_ntt2x2(bram *ram, bram *mul_ram, enum OPERATION mode, enum MAPPING 
             fifo_b[DEPT_B] = {0},
             fifo_c[DEPT_C] = {0},
             fifo_d[DEPT_D] = {0},
-            fifo_w[DEPT_W] = {0};
+            fifo_w[4 * DEPT_W] = {0};
 
-    int32_t fa, fb, fc, fd, fi, fw;
+    // int32_t fa, fb, fc, fd;
+    int32_t fi, fw;
 
-    // int i1, i2, i3, i4;
+    int i1, i2, i3, i4;
+    int i1_base, i2_base, i3_base, i4_base;
+    int32_t w[4];
     int ram_i, bound, addr;
 
     int count = 0;
 
     // --------------------
     int s_array[4] = {4, 2, 0, 4};
+    int s;
     int start_ntt = 0;
-    // int32_t a, b, c, d;
-    // a = -1;
-    // b = -1;
-    // c = -1;
-    // d = -1;
+    int32_t a, b, c, d;
+    int32_t tb, tc;
     // --------------------
 
     switch (mode)
@@ -210,7 +220,7 @@ void forward_ntt2x2(bram *ram, bram *mul_ram, enum OPERATION mode, enum MAPPING 
         break;
 
     case FORWARD_NTT_MODE:
-        bound = DILITHIUM_LOGN;
+        bound = 8;
         printf("FOFWARD NTT is not ready\n");
         break;
 
@@ -220,73 +230,172 @@ void forward_ntt2x2(bram *ram, bram *mul_ram, enum OPERATION mode, enum MAPPING 
         break;
     }
 
-    (void) mul_ram;
-    (void) fw; 
-    (void) fi;
-    (void) decode;
+    int once = 0;
 
-    for (int si = 0; si < bound; ++si)
+    (void)mul_ram;
+    (void)fw;
+    (void)fi;
+    (void)decode;
+    int last = 0;
+
+    int32_t line_in[4], line_out[4];
+
+    for (int si = 0; si < bound; si += 2)
     {
-        for (int j = 0; j < 1 << s_array[si]; j++)
+        s = s_array[si >> 1];
+        for (int j = 0; j < 1 << s; j++)
         {
-            for (int k = 0; j < DILITHIUM_N / 4; j += 1 << s_array[si])
+            for (int k = 0; k < DILITHIUM_N / 4; k += 1 << s)
             {
-                
+                if (si < 6)
+                {
+                    if (k == 0 && mode == FORWARD_NTT_MODE)
+                    {
+                        i1_base = i2_base = 1 << si;
+                        i3_base = (1 << (si + 1));
+                        i4_base = (1 << (si + 1)) + 1;
+
+                        i1 = i1_base;
+                        i2 = i2_base;
+                        i3 = i3_base;
+                        i4 = i4_base;
+                    }
+                }
+                else if (!once)
+                {
+                    i1_base = i2_base = 1 << si;
+                    i3_base = (1 << (si + 1));
+                    i4_base = (1 << (si + 1)) + 1;
+
+                    i1 = i1_base;
+                    i2 = i2_base;
+                    i3 = i3_base;
+                    i4 = i4_base;
+                    once = 1;
+                }
+
+                if (mode == FORWARD_NTT_MODE)
+                {
+                    w[0] = zetas_barret[i1];
+                    w[1] = zetas_barret[i2];
+                    w[2] = zetas_barret[i3];
+                    w[3] = zetas_barret[i4];
+
+                    // w[0] = i1;
+                    // w[1] = i2;
+                    // w[2] = i3;
+                    // w[3] = i4;
+                }
+
+                PIPO(line_out, DEPT_W * 4, fifo_w, w);
+
                 // Load then store to FIFO
                 addr = k + j;
 
                 ram_i = addr;
 
-                fa = FIFO(DEPT_A, fifo_a, -1);
-                fb = FIFO(DEPT_B, fifo_b, -1);
-                fc = FIFO(DEPT_C, fifo_c, -1);
-                fd = FIFO(DEPT_D, fifo_d, -1);
-                // fi = FIFO(DEPT_W, fifo_i, -1);
-                // fw = FIFO(DEPT_W, fifo_w, -1);
+                if (si >= 6)
+                {
+                    last++;
+                }
 
-                write_fifo(fifo_a, fifo_b, fifo_c, fifo_d, count, ram, ram_i);
+                fi = FIFO(DEPT_W, fifo_i, ram_i);
+                write_fifo(&a, &b, &c, &d, fifo_a, fifo_b, fifo_c, fifo_d, count, ram, ram_i);
 
-                start_ntt = count == DEPT_W;
-                
+                if (count == DEPT_W)
+                    start_ntt = 1;
+
                 count++;
-                
 
                 // Compute
 
                 if (start_ntt)
                 {
-                    printf("%d, %d, %d, %d\n", fa, fb, fc, fd);
-                    printf("--------\n");
+                    // printf("[%d] %d, %d, %d, %d\n", fi,  a, b, c, d);
+                    // print_array(line_out, 4, "hls w:");
+                    buttefly_circuit(&a, &b, &c, &d, line_out[0], line_out[1], line_out[2], line_out[3], FORWARD_NTT_MODE);
+                    // 
+                    
+                    if (last > 4)
+                    {
+                        tb = b;
+                        tc = c;
+                    }
+                    else
+                    {
+                        tb = b;
+                        tc = c;
+                    }
+                    write_ram(ram, fi, a, tb, tc, d);  
                 }
 
                 // printf("w: %2d - %2d - %2d - %2d\n", i1, i2, i3, i4);
 
-                print_array(fifo_d, DEPT_D, "FIFO_A");
-                print_array(fifo_c, DEPT_C, "FIFO_B");
-                print_array(fifo_b, DEPT_B, "FIFO_C");
-                print_array(fifo_a, DEPT_A, "FIFO_D");
-                print_array(fifo_i, DEPT_W, "FIFO_I");
-                print_array(fifo_w, DEPT_W, "FIFO_W");
-                printf("--------%d\n", ram_i);
+                // print_array(fifo_d, DEPT_D, "FIFO_A");
+                // print_array(fifo_c, DEPT_C, "FIFO_B");
+                // print_array(fifo_b, DEPT_B, "FIFO_C");
+                // print_array(fifo_a, DEPT_A, "FIFO_D");
+
+                // print_array(fifo_i, DEPT_W, "FIFO_I");
+                // print_array(fifo_w, DEPT_W, "FIFO_W");
+                // printf("--------%d\n", ram_i);
 
                 // printf("=====================\n\n");
+
+                if (mode == FORWARD_NTT_MODE)
+                {
+                    int mask1 = (2 << si) - 1;
+                    int mask2 = (2 << (si + 1)) - 1;
+                    if (si < 6)
+                    {
+                        i1 = max(i1_base, (i1 + 1) & mask1);
+                        i2 = max(i2_base, (i2 + 1) & mask1);
+                        i3 = max(i3_base, (i3 + 2) & mask2);
+                        i4 = max(i4_base, (i4 + 2) & mask2);
+                    }
+                    else
+                    {
+                        // printf("mask: %d - %d\n", mask1, mask2);
+                        i1 = (i1 + 1) & mask1;
+                        i2 = (i2 + 1) & mask1;
+                        i3 = (i3 + 2) & mask2;
+                        i4 = (i4 + 2) & mask2;
+                    }
+                }
             }
+            // printf("=====================\n");
         }
+    }
+    for (int i = 0; i < DEPT_W; i++)
+    {
+        fi = FIFO(DEPT_W, fifo_i, 0);
+        PIPO(line_out, DEPT_W * 4, fifo_w, w);
+        write_fifo(&a, &b, &c, &d, fifo_a, fifo_b, fifo_c, fifo_d, count, ram, ram_i);
+        buttefly_circuit(&a, &b, &c, &d, line_out[0], line_out[1], line_out[2], line_out[3], FORWARD_NTT_MODE);
+        // Writeback
+        write_ram(ram, fi, a, b, c, d);
     }
 
     // for (int i = 0; i < DEPT_I+1; i++)
     // {
     //     printf("=====END\n");
     //     move_fifo(&fa, &fb, &fc, &fd, &fi, &fw,
-    //                 fifo_a, fifo_b, fifo_c, fifo_d, 
+    //                 fifo_a, fifo_b, fifo_c, fifo_d,
     //                 fifo_i, ram_i, fifo_w, w);
     //     printf("%d, %d, %d, %d\n", fa, fb, fc, fd);
 
+    // print_array(fifo_d, DEPT_D, "FIFO_A");
+    // print_array(fifo_c, DEPT_C, "FIFO_B");
+    // print_array(fifo_b, DEPT_B, "FIFO_C");
+    // print_array(fifo_a, DEPT_A, "FIFO_D");
 
-    //     // print_array(fifo_d, DEPT_D, "FIFO_A");
-    //     // print_array(fifo_c, DEPT_C, "FIFO_B");
-    //     // print_array(fifo_b, DEPT_B, "FIFO_C");
-    //     // print_array(fifo_a, DEPT_A, "FIFO_D");
-    //     printf("--------%d\n", ram_i);
+    // print_array(fifo_i, DEPT_W, "FIFO_I");
+    // print_array(fifo_w, DEPT_W, "FIFO_W");
+        // printf("--------%d\n", fi);
     // }
+    printf("==========\n\n\n");
 }
+
+/* 
+Correct 2 layers
+ */
