9b2b699db324810cd270d82d0c60c0f218d19789
cothan <cothanhahaha123@gmail.com>
2021-11-28 05:59:52 -0500

    improve index cal

--- falcon/invntt2x2_ref.c
+++ falcon/invntt2x2_ref.c
@@ -20,62 +20,70 @@
   a = div2(a);                       \
   b = ((uint32_t)t * z) % FALCON_Q;
 
-
 void invntt2x2_ref(uint16_t a[FALCON_N])
 {
-  uint16_t len, last;
+  uint16_t len, leftover;
   uint16_t a1, b1, a2, b2;
   uint16_t t1, t2;
   uint16_t k1[2], k2;
   uint16_t zeta1[2], zeta2;
-  for (unsigned l = 0; l < FALCON_LOGN; l += 2)
+  int l;
+  for (l = 0; l < FALCON_LOGN - (FALCON_LOGN & 1); l += 2)
   {
     len = 1 << l;
-    last = (l == (FALCON_LOGN - 2));
+
     for (unsigned i = 0; i < FALCON_N; i += 1 << (l + 2))
     {
-      k1[0] = (FALCON_N >> l) - 1 - (i >> (l + 1));
+      k1[0] = ((FALCON_N - i/2)>> l) - 1;
       k1[1] = k1[0] - 1;
-      k2 = (FALCON_N >> (l + 1)) - 1 - (i >> (l + 2));
+      // k2 = (FALCON_N >> (l + 1)) - 1 - (i >> (l + 2));
+      k2 = ((FALCON_N - i/2) >> (l + 1)) - 1;
       zeta1[0] = FALCON_Q - zetas_barrett[k1[0]];
       zeta1[1] = FALCON_Q - zetas_barrett[k1[1]];
       zeta2 = FALCON_Q - zetas_barrett[k2];
 
-      // printf("[======]%d\n", i);
       for (unsigned j = i; j < i + len; j++)
       {
         a1 = a[j];
-        b1 = a[j + len];
-        a2 = a[j + 2 * len];
+        a2 = a[j + len];
+        b1 = a[j + 2 * len];
         b2 = a[j + 3 * len];
 
         // Left
-        // a1 - b1, a2 - b2
-        gsbf_div2(a1, b1, zeta1[0], t1);
-        gsbf_div2(a2, b2, zeta1[1], t2);
+        // a1 - a2, b1 - b2
+        gsbf_div2(a1, a2, zeta1[0], t1);
+        gsbf_div2(b1, b2, zeta1[1], t2);
 #if DEBUG == 1
         printf("[%d]: %u, %u = %u, %u | %u\n", len, j, j + len,
-               a1, b1, k1[0]);
+               a1, a2, k1[0]);
         printf("[%d]: %u, %u = %u, %u | %u\n", len, j + 2 * len, j + 3 * len,
-               a2, b2, k1[1]);
+               b1, b2, k1[1]);
 #endif
+
         // Right
-        // a1 - a2, b1 - b2
-        gsbf_div2(a1, a2, zeta2, t1);
-        gsbf_div2(b1, b2, zeta2, t2);
+        // a1 - b1, a2 - b2
+        gsbf_div2(a1, b1, zeta2, t1);
+        gsbf_div2(a2, b2, zeta2, t2);
 #if DEBUG == 1
         printf("[%d]: %u, %u = %u, %u | %u\n", 2 * len, j, j + 2 * len,
-               a1, a2, k2);
+               a1, b1, k2);
         printf("[%d]: %u, %u = %u, %u | %u\n", 2 * len, j + len, j + 3 * len,
-               b1, b2, k2);
+               a2, b2, k2);
 #endif
         a[j] = a1;
-        a[j + len] = b1;
-        a[j + 2 * len] = a2;
+        a[j + len] = a2;
+        a[j + 2 * len] = b1;
         a[j + 3 * len] = b2;
       }
     }
   }
+
+  for (l = FALCON_LOGN; l > FALCON_LOGN - (FALCON_LOGN & 1); l--)
+  {
+    len = FALCON_N >> l;
+    // leftover = (l == )
+  }
+  // End function
 }
 
 void invntt(uint16_t a[FALCON_N])
@@ -110,19 +118,17 @@ void invntt(uint16_t a[FALCON_N])
 
         m = a[j];
         n = a[j + len];
-        // printf("%d: %u, %u = %u, %u | %u\n", len, j, j + len, m, n, k);
+#if DEBUG == 5
+        printf("%d: %u, %u = %u, %u | %u\n", len, j, j + len, m, n, k);
+#endif
       }
     }
   }
 
   for (j = 0; j < FALCON_N; ++j)
   {
-    // This work, 1024^-1 mod Q = 12277
+    // This work, but f is not same as in ref code
     a[j] = ((uint32_t)f * a[j]) % FALCON_Q;
-
-    // This doesn't work, no idea why they pick f like this
-    // f is multiple of 2, so shift and reduction
-    // a[j] = ((uint32_t)f * a[j]) % FALCON_Q;
   }
 }
 
@@ -143,9 +149,9 @@ int main()
       a_gold[i] = tmp;
     }
 
-    invntt(a_gold);
-    // printf("=======\n");
     invntt2x2_ref(a);
+    // printf("=======\n");
+    invntt(a_gold);
 
     for (int i = 0; i < FALCON_N; i++)
     {
