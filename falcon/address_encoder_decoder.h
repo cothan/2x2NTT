cacadc6245d0c79cf7c9f15f2fdcf58be847c7f6
cothan <cothanhahaha123@gmail.com>
2021-12-08 02:48:39 -0500

    clean up

--- falcon/address_encoder_decoder.h
+++
@@ -1,9 +0,0 @@
-#ifndef ADDRESS_ENCODER_DECODER_H
-#define ADDRESS_ENCODER_DECODER_H
-
-int resolve_address(enum MAPPING mapping, int addr);
-
-void resolve_twiddle(uint16_t tw_i[4], uint16_t *last, uint16_t tw_base_i[4],
-                    const int k, const int s, enum OPERATION mode);
-
-#endif
